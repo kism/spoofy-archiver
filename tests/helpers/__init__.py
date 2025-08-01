a741d0676adff3a1762da447481da9caa235ded9
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-08-01 16:02:41 +0800

    fix

--- tests/helpers/__init__.py
+++ tests/helpers/__init__.py
@@ -6,7 +6,8 @@ import pytest
 def assert_no_warnings_in_caplog(caplog: pytest.LogCaptureFixture) -> None:
     """Assert that there are no WARNING or above logs in the caplog."""
     warning_records = [record for record in caplog.records if record.levelno >= logging.WARNING]
+    nl = "\n  "  # no \n in f string in python 3.11
     assert len(warning_records) == 0, (
         f"{len(warning_records)} warning(s) in log, run with -vvv to see \n  "
-        f"{'\n  '.join(record.message for record in warning_records)}"
+        f"{nl.join(record.message for record in warning_records)}"
     )
