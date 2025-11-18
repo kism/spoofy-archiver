52f077fde947c008c8bef112583f8b94b4a0c9ae
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-11-18 15:17:58 +0800

    stable

--- spoofy_archiver/__init__.py
+++ spoofy_archiver/__init__.py
@@ -2,6 +2,6 @@
 
 from spoofy_archiver.utils.logger import setup_logger
 
-__version__ = "1.0.8b1"  # This is the version of the app, used in pyproject.toml, enforced in a test.
+__version__ = "1.0.8"  # This is the version of the app, used in pyproject.toml, enforced in a test.
 
 setup_logger()
